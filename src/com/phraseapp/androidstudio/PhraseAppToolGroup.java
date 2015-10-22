5b4b30e08725011dd2fd75c375388a24fbd2420e
Andre Brühl <andre.bruehl@dynport.de>
2015-10-22 10:56:44 +0200

    moved actions into tools menu

---
+++ src/com/phraseapp/androidstudio/PhraseAppToolGroup.java
@@ -0,0 +1,13 @@
+package com.phraseapp.androidstudio;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by kolja on 21.10.15.
+ */
+public class PhraseAppToolGroup extends DefaultActionGroup {
+
+}
